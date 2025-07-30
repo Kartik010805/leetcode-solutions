f09ccf7fe9ea2039518c3ffc7b91e37caf7d8910
Kartik010805 <kartikgupta649@gmail.com>
2025-07-31 00:52:31 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,14 @@
+// Last updated: 7/31/2025, 12:52:31 AM
+public class Solution { // Class name must be 'Solution' as per LeetCode
+    
+    public int[] twoSum(int[] nums, int target) { // LeetCode calls this method
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j}; // Return indices if found
+                }
+            }
+        }
+        return new int[]{-1, -1}; // Ensure the return array is always of size 2
+    }
+}
