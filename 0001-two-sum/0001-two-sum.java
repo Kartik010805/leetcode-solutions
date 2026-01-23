0193627dd45cb93e3e68b723d91ac635e557a65a
Kartik010805 <kartikgupta649@gmail.com>
2026-01-23 16:08:52 +0530

    Time: 2 ms (99.14%), Space: 46.5 MB (81.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,19 @@
-public class Solution { // Class name must be 'Solution' as per LeetCode
-    
-    public int[] twoSum(int[] nums, int target) { // LeetCode calls this method
+
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j}; // Return indices if found
-                }
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
             }
+
+            map.put(nums[i], i);
         }
-        return new int[]{-1, -1}; // Ensure the return array is always of size 2
+
+        return new int[]{-1, -1};
     }
 }
