32d6f7306a82db4165dc3d3a49e59b37b7832b7d
Kartik010805 <kartikgupta649@gmail.com>
2026-06-23 22:02:30 +0530

    Time: 1 ms (100%), Space: 44 MB (71.74%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,46 +1,48 @@
+
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> queue=new ArrayDeque<>();
-        boolean [][]visited=new boolean[grid.length][grid[0].length];
-        int fresh=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2) {
-                    queue.offer(new int[]{i,j,0});
-                    visited[i][j]=true;
-                }else if(grid[i][j]==1){
+        Queue<int[]> q = new ArrayDeque<>();
+        int fresh = 0;
+        int time = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
-        int time=0;
-        while(!queue.isEmpty()){
-            int[] curr=queue.poll();
-            int r=curr[0];
-            int c=curr[1];
-            int t=curr[2];
-            time=Math.max(time,t);
-            if(r-1>=0 && !visited[r-1][c] && grid[r-1][c]==1){
-                visited[r-1][c]=true;
-                fresh--;
-                queue.offer(new int[]{r-1,c,t+1});
-            }
-            if(r+1<grid.length && !visited[r+1][c] && grid[r+1][c]==1){
-                visited[r+1][c]=true;
-                fresh--;
-                queue.offer(new int[]{r+1,c,t+1});
-            }
-            if(c-1>=0 && !visited[r][c-1] && grid[r][c-1]==1){
-                visited[r][c-1]=true;
-                fresh--;
-                queue.offer(new int[]{r,c-1,t+1});
-            }
-            if(c+1<grid[0].length && !visited[r][c+1] && grid[r][c+1]==1){
-                visited[r][c+1]=true;
-                fresh--;
-                queue.offer(new int[]{r,c+1,t+1});
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+            time++;
+
+            while (size-- > 0) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + dr[i];
+                    int nc = c + dc[i];
+
+                    if (nr >= 0 && nr < grid.length &&
+                        nc >= 0 && nc < grid[0].length &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                    }
+                }
             }
         }
-        return fresh==0?time:-1;
+
+        return fresh == 0 ? time : -1;
     }
 }
