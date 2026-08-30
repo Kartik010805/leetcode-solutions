a73fd4f4309fd6af18dc9039be7a2fd5766b415a
Kartik010805 <kartikgupta649@gmail.com>
2026-08-31 00:46:22 +0530

    Time: 3 ms (88.01%), Space: 51 MB (98.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -6,8 +6,9 @@ class Solution {
       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(grid[i][j]=='1'){
-                island++;
+                
                 dfs(grid,i,j);
+                island++;
                 
             }
         }
