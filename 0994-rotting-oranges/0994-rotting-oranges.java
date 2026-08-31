963a7ba541181f129c7fa564901721c27da8afc1
Kartik010805 <kartikgupta649@gmail.com>
2026-08-31 19:00:13 +0530

    Time: 2 ms (89.4%), Space: 43.9 MB (92.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,48 +1,60 @@
-
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new ArrayDeque<>();
-        int fresh = 0;
-        int time = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
-        }
-
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-
-        while (!q.isEmpty() && fresh > 0) {
-            int size = q.size();
-            time++;
-
-            while (size-- > 0) {
-                int[] curr = q.poll();
-                int r = curr[0];
-                int c = curr[1];
-
-                for (int i = 0; i < 4; i++) {
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-
-                    if (nr >= 0 && nr < grid.length &&
-                        nc >= 0 && nc < grid[0].length &&
-                        grid[nr][nc] == 1) {
-
-                        grid[nr][nc] = 2;
-                        fresh--;
-                        q.offer(new int[]{nr, nc});
-                    }
-                }
-            }
-        }
-
-        return fresh == 0 ? time : -1;
-    }
+class Solution { 
+    public int orangesRotting(int[][] grid) { 
+        int n = grid.length; 
+        int m = grid[0].length; 
+ 
+        int time = 0; 
+        int fresh = 0; 
+ 
+        Queue<int[]> queue = new LinkedList<>(); 
+ 
+        for(int i = 0; i < n; i++) { 
+            for(int j = 0; j < m; j++) { 
+                if(grid[i][j] == 1) { 
+                    fresh++; 
+                } 
+ 
+                if(grid[i][j] == 2) { 
+                    queue.offer(new int[]{i, j}); 
+                } 
+            } 
+        } 
+ 
+        int[] x = {-1, 1, 0, 0}; 
+        int[] y = {0, 0, -1, 1}; 
+ 
+        while(!queue.isEmpty() && fresh > 0) { 
+            int size = queue.size(); 
+ 
+            for(int i = 0; i < size; i++) { 
+                int[] curr = queue.poll(); 
+ 
+                int r = curr[0]; 
+                int c = curr[1]; 
+ 
+                for(int k = 0; k < 4; k++) { 
+                    int nr = r + x[k]; 
+                    int nc = c + y[k]; 
+ 
+                    if(nr >= 0 && nr < n && 
+                       nc >= 0 && nc < m && 
+                       grid[nr][nc] == 1) { 
+ 
+                        grid[nr][nc] = 2; 
+                        fresh--; 
+ 
+                        queue.offer(new int[]{nr, nc}); 
+                    } 
+                } 
+            } 
+ 
+            time++; 
+        } 
+ 
+        if(fresh > 0) { 
+            return -1; 
+        } 
+ 
+        return time; 
+    } 
 }
