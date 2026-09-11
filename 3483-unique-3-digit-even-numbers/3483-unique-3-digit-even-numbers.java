a9c2c21625e7e2e6f96a692054e19350cbf0c757
Kartik010805 <kartikgupta649@gmail.com>
2026-09-11 18:12:30 +0530

    Time: 8 ms (32.13%), Space: 46.9 MB (15.2%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] count = new int[10];
+
+        for (int d : digits) {
+            count[d]++;
+        }
+
+        int uniqueCount = 0;
+
+        for (int num = 100; num < 1000; num += 2) {
+            if (canForm(num, count)) {
+                uniqueCount++;
+            }
+        }
+
+        return uniqueCount;
+    }
+
+    private boolean canForm(int num, int[] baseCount) {
+        int[] numCount = new int[10];
+
+        while (num > 0) {
+            int digit = num % 10;
+            numCount[digit]++;
+
+            if (numCount[digit] > baseCount[digit]) {
+                return false;
+            }
+
+            num /= 10;
+        }
+
+        return true;
+    }
+}
